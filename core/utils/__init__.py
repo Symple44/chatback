af6bd77f98d415f0ea802105d34d43c41e3ad602
Symple44 <nicolas.dubain@gmail.com>
2024-12-21 11:07:40 +0100

    Update __init__.py

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,5 +1,10 @@
 # core/utils/__init__.py
 from .logger import get_logger
 from .metrics import metrics
+from .system_optimizer import SystemOptimizer
 
-__all__ = ['get_logger', 'metrics']
+__all__ = [
+    'get_logger',
+    'metrics',
+    'SystemOptimizer'
+]
