4bdf9571e374a53b1ca206606e3aacac374d2b94
Symple44 <nicolas.dubain@gmail.com>
2024-12-22 09:28:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ try:
         def default(self, obj):
             if isinstance(obj, datetime):
                 return obj.isoformat()
+            if isinstance(obj, UUID):
+                return str(obj)
             return super().default(obj)
     
     class CustomJSONResponse(JSONResponse):
