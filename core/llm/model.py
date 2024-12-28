16ffa4829282b9a9fc36ff6bb44fd7f74d608edf
Symple44 <nicolas.dubain@gmail.com>
2024-12-28 17:34:14 +0100

    Update model.py

--- core/llm/model.py
+++ core/llm/model.py
@@ -209,7 +209,7 @@ class ModelInference:
                     bnb_4bit_compute_dtype=torch.float16,
                     bnb_4bit_use_double_quant=True,
                     bnb_4bit_quant_type="nf4",
-                    bnb_4bit_quant_storage="auto"
+                    bnb_4bit_quant_storage_dtype=torch.float16
                 )
     
             # Chargement progressif
