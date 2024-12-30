12648867f76cbd3da7c1b73d1c95abb8da0d73a4
Symple44 <nicolas.dubain@gmail.com>
2024-12-30 15:36:21 +0100

    Update config.py

--- core/config.py
+++ core/config.py
@@ -146,13 +146,14 @@ class Settings(BaseSettings):
     # Templates
     CHAT_TEMPLATE: str = """System: {system}\nQuestion: {query}\nContexte: {context}\n\nRéponse:"""
     SYSTEM_PROMPT: ClassVar[str] = (
-        "Je suis {APP_NAME}, un assistant IA conçu pour fournir des réponses claires, "
+        "Je suis {app_name}, un assistant IA conçu pour fournir des réponses claires, "
         "pertinentes et détaillées. Mon objectif principal est de m'appuyer sur les documents "
         "fournis pour répondre précisément aux questions posées.\n"
         "Si les informations nécessaires ne sont pas disponibles dans les documents, je fournirai "
         "des explications basées sur mes connaissances générales. Cependant, les données existantes "
         "seront toujours ma priorité pour garantir des réponses contextualisées et fiables."
-    )
+    ).format(app_name=APP_NAME)
+
     
     SYSTEM_MESSAGES: ClassVar[Dict[str, str]] = {
         "welcome": "Bienvenue ! Comment puis-je vous aider ?",
