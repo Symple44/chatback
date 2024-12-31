310d717b3be66ff78c106b93829f19b2ce8b62a4
Symple44 <nicolas.dubain@gmail.com>
2024-12-31 12:58:49 +0100

    Update base.py

--- core/database/base.py
+++ core/database/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import AsyncAdaptedQueuePool
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Text
 import asyncio
 from datetime import datetime
 
