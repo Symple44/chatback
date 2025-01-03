bbf6a43c8daf6876d35b9b9e5030e99a8314925e
Symple44 <nicolas.dubain@gmail.com>
2025-01-03 10:10:31 +0100

    Update __init__.py

--- core/vectorstore/__init__.py
+++ core/vectorstore/__init__.py
@@ -1,14 +1,12 @@
 # core/vectorstore/__init__.py
 from .elasticsearch_client import ElasticsearchClient
 from .search import SearchManager
-from .index import IndexManager
 from .query_builder import QueryBuilder
 from .response_formatter import ResponseFormatter
 
 __all__ = [
     'ElasticsearchClient',
     'SearchManager',
-    'IndexManager',
     'QueryBuilder',
     'ResponseFormatter'
 ]
