99df95dea1f3a6037ae6e3dd7f66da35e59229ae
SYMPLE <nicolas.dubain@gmail.com>
2025-02-14 09:21:50 +0100

    Update models.py

--- core/config/models.py
+++ core/config/models.py
@@ -380,9 +380,11 @@ SUMMARIZER_MODELS = {
             "torch_dtype": torch.float16,
             "max_memory": {0: "4GiB", "cpu": "8GB"},
             "quantization_config": {
+                "load_in_4bit": True,
                 "bnb_4bit_compute_dtype": torch.float16,
                 "bnb_4bit_quant_type": "nf4",
-                "bnb_4bit_use_double_quant": True
+                "bnb_4bit_use_double_quant": True,
+                "quant_method": "bitsandbytes"
             }
         },
         "generation_config": {
@@ -422,9 +424,11 @@ SUMMARIZER_MODELS = {
             "torch_dtype": torch.float16,
             "max_memory": {0: "4GiB", "cpu": "8GB"},
             "quantization_config": {
+                "load_in_4bit": True,
                 "bnb_4bit_compute_dtype": torch.float16,
                 "bnb_4bit_quant_type": "nf4",
-                "bnb_4bit_use_double_quant": True
+                "bnb_4bit_use_double_quant": True,
+                "quant_method": "bitsandbytes"
             }
         },
         "generation_config": {
