2982caf652685578cd4fa1e75314226b605ae42d
SYMPLE <nicolas.dubain@gmail.com>
2025-02-14 08:39:40 +0100

    up

--- core/config/models.py
+++ core/config/models.py
@@ -22,6 +22,7 @@ AVAILABLE_MODELS = {
             "torch_dtype": torch.float16,
             "max_memory": {0: "16GiB", "cpu": "12GB"},
             "quantization_config": {
+                "load_in_4bit": True,
                 "bnb_4bit_compute_dtype": torch.float16,
                 "bnb_4bit_quant_type": "nf4",
                 "bnb_4bit_use_double_quant": True,
@@ -96,6 +97,7 @@ AVAILABLE_MODELS = {
             "torch_dtype": torch.float16,
             "max_memory": {0: "20GiB", "cpu": "24GB"},
             "quantization_config": {
+                "load_in_4bit": True,
                 "bnb_4bit_compute_dtype": torch.float16,
                 "bnb_4bit_quant_type": "nf4",
                 "bnb_4bit_use_double_quant": True,
